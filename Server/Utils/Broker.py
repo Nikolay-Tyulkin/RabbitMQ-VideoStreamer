f003083ad7347e0eeabb68efbd88919ddcdfca09
Nikolay-Tyulkin <tyulkin.np@yandex.ru>
2023-03-27 19:13:55 +0000

    update

--- Server/Utils/Broker.py
+++ Server/Utils/Broker.py
@@ -33,10 +33,10 @@ class Broker:
 
         self.channel.queue_bind(exchange=self.exchange, queue=queue_name)
 
-        print(' [*] Waiting for logs. To exit press CTRL+C')
+        #print(' [*] Waiting for logs. To exit press CTRL+C')
 
-        def callback(self, ch, method, properties, body):
-            print(" [x] image has been recieved")
+        def callback(ch, method, properties, body):
+            #print(" [x] image has been recieved")
             image = base64.b64decode(body)
             image = np.frombuffer(image, np.uint8)
             image = cv2.imdecode(image, cv2.IMREAD_COLOR)
@@ -48,8 +48,9 @@ class Broker:
 
     def getImage(self):
         image = self.imageBuffer
-        self.imageBuffer = None
-        return image
+        #print(image.shape)
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
     
 
         
--- Server/main.py
+++ Server/main.py
@@ -1,7 +1,12 @@
 # import the necessary packages
 from flask import Flask, render_template, Response
 from camera import VideoCamera
+from Utils.Broker import Broker
+
 app = Flask(__name__)
+broker = Broker()
+broker.startRecieving()
+
 @app.route('/')
 def index():
     # rendering webpage
@@ -9,12 +14,12 @@ def index():
 def gen(camera):
     while True:
         #get camera frame
-        frame = camera.get_frame()
+        frame = broker.getImage()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(VideoCamera()),
+    return Response(gen(broker),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__ == '__main__':
     # defining server ip address and port
