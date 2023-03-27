1401914cecd2f9207c3ee9a7cd07c0b990818086
Nikolay-Tyulkin <tyulkin.np@yandex.ru>
2023-03-27 18:22:42 +0000

    Update

--- Client/[test] RabbitMQ-sender.py
+++ Client/[test] RabbitMQ-sender.py
@@ -6,24 +6,21 @@ import numpy as np
 import base64
 import time
 
-image = cv2.imread(r'./Client/1L.jpg')
+image = cv2.imread(r'RabbitMQ-VideoStreamer/Client/1L.jpg')
 print(image.shape)
-image = cv2.resize(image, (480, 480))
+image = cv2.resize(image, (480, 640))
 image = cv2.imencode('.jpg', image)[1]
 image = base64.b64encode(image)
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='84.201.177.253'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="84.201.143.216"))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='logs', exchange_type='fanout')
+channel.exchange_declare(exchange='video-streamer', exchange_type='fanout')
 
 message = image
-for i in range(100):
-    time.sleep(0.1)
-    channel.basic_publish(exchange='logs', routing_key='', body=message)
+
+channel.basic_publish(exchange='video-streamer', routing_key='', body=message)
 print(" [x] Sent Done!" )
 connection.close()
-
-
 
 
