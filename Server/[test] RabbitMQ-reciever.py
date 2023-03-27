80e55b2a2f88d212b6911392a7910c3283fd8173
Nikolay-Tyulkin <tyulkin.np@yandex.ru>
2023-03-27 17:26:06 +0000

    Update

--- Server/[test] RabbitMQ-reciever.py
+++ Server/[test] RabbitMQ-reciever.py
@@ -1,14 +1,29 @@
+
 import pika
+import cv2
+import numpy as np
+import base64
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('84.201.177.253'))
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='84.201.143.216'))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+
+channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(exchange='logs', queue=queue_name)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+    print(" [x] image has been recieved")
+    image = base64.b64decode(body)
+    image = np.frombuffer(image, np.uint8)
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    cv2.imwrite("./reciviedImage.jpg", image)
+channel.basic_consume(
+    queue=queue_name, on_message_callback=callback, auto_ack=True)
 
-channel.basic_consume(queue='hello',
-                      auto_ack=True,
-                      on_message_callback=callback)
-
-print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
